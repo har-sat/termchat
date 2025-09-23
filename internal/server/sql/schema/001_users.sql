dfa8a77869135401b38fae9173507c9770dd303b
har-sat <harsat.ponnusamy@gmail.com>
2025-09-24 00:11:06 +0530

    database schema

--- internal/server/sql/schema/001_users.sql
+++ internal/server/sql/schema/001_users.sql
@@ -7,5 +7,25 @@ CREATE TABLE users (
     updated_at TIMESTAMP NOT NULL
 );
 
+
+CREATE TABLE rooms (
+    id UUID PRIMARY KEY,
+    name VARCHAR(32) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    creator_id UUID NOT NULL REFERENCES users(id),
+    owner_id UUID NOT NULL REFERENCES users(id),
+);
+
+CREATE TABLE messages (
+    id UUID PRIMARY KEY,
+    data VARCHAR(1000) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    sender_id UUID NOT NULL REFERENCES users(id),
+    room_id UUID NOT NULL REFERENCES rooms(id),
+);
+
 -- +goose Down
-DROP TABLE users
+DROP TABLE users;
+DROP TABLE rooms;
+DROP TABLE messages;
